840be02ce72a6bd97d81333bfad5c674e047634d
safraoui khalil <khalil.safraoui.92@outlook.fr>
2025-03-07 16:11:19 +0100

    Redesing Sign in and Sign up and add role option  in sing up  and manage it

---
+++ SK.Solution/SK.Solution/Utility/RoleType.cs
@@ -0,0 +1,8 @@
+﻿namespace SK.Solution.Utility
+{
+    public static class RoleType
+    {
+        public static string Admin = "Admin";
+        public static string Customer = "Customer";
+    }
+}
