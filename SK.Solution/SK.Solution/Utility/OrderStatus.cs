08978c8954473d2d7da6a3fb805a30c9f69cb369
safraoui khalil <khalil.safraoui.92@outlook.fr>
2025-03-08 15:40:58 +0100

    Displayed the order in the cart page

---
+++ SK.Solution/SK.Solution/Utility/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace SK.Solution.Utility
+{
+    public static class OrderStatus
+    {
+        public static string StatusPending = "Pending";
+        public static string StatusReadyForPickUp = "ReadyForPickUp";
+        public static string StatusCompleted = "Completed";
+        public static string StatusCancelled = "Cancelled";
+    }
+}
