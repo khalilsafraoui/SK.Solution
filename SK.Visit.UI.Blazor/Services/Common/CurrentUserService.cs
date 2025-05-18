a581aa9f1d55af688d4df040bf67a68f2c5f1673
safraoui khalil <khalil.safraoui.92@outlook.fr>
2025-05-18 18:16:27 +0200

    trying to catch an issue related to claims in the server

--- SK.Visit.UI.Blazor/Services/Common/CurrentUserService.cs
+++ SK.Visit.UI.Blazor/Services/Common/CurrentUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using SK.Visit.Application.Interfaces.Common;
 using System.Security.Claims;
 
@@ -9,17 +10,26 @@ namespace SK.Visit.UI.Blazor.Common
     public class CurrentUserService : ICurrentUserService
     {
         private readonly IHttpContextAccessor _contextAccessor;
+        private readonly ILogger<CurrentUserService> _logger;
 
-        public CurrentUserService(IHttpContextAccessor contextAccessor)
+        public CurrentUserService(IHttpContextAccessor contextAccessor, ILogger<CurrentUserService> logger)
         {
             _contextAccessor = contextAccessor;
+            _logger = logger;
         }
 
         public Guid UserId
         {
             get
             {
-                var id = _contextAccessor.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier).Value;
+                var user = _contextAccessor.HttpContext?.User;
+                var id = user?.FindFirst(ClaimTypes.NameIdentifier).Value;
+                if (string.IsNullOrWhiteSpace(id))
+                {
+                    _logger.LogInformation("Authenticated: {auth}", user?.Identity?.IsAuthenticated ?? false);
+                    _logger.LogInformation("ClaimTypes.NameIdentifier: {id}", id ?? "null");
+                    throw new InvalidOperationException("The user ID claim is missing from the current user context.");
+                }
                 return Guid.Parse(id);
             }
         }
